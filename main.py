6764426eb5651795f69c83a215cb7adb928d2c63
Psalms_Martial <109070870+PuffMartial@users.noreply.github.com>
2022-10-26 10:38:05 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+name = "kevin";
+job = "fullstack developer";
+location = "capetown, south africa";
+skills = "programing cooking swimming";
+print("BIO")
+print("My name is " + name + "im a " + job +"i stay in" + location + "i can do " + skills)
